3dea6e31c469bbe7b0cb9d3d4b33af7687e452cd
99x-dgCandidate-Saranga-1737099400352-at-675482622082 <sarangasama@gamail.com>
2025-09-18 00:18:27 +0530

    Complete the verify otp function

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const authController = require('../controller/authController.js')
+const userAuth = require('../middleware/userAuth.js')
 
 const router = express.Router();
 
 
 router.post('/register', authController.register)
 router.post('/login', authController.login)
+router.post('/logout', authController.logout)
+router.post('/send-verify-otp', userAuth, authController.verifyOtp)
 
 
 module.exports = router;
