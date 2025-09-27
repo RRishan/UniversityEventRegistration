ac372d3a24f07815b793cb89ebac2a71b0d5ed55
nivaethan-dev <fc115565@foc.sjp.ac.lk>
2025-09-27 20:15:59 +0530

    Tests updated

--- backend/tests/utils/commonMocks.js
+++ backend/tests/utils/commonMocks.js
@@ -1,40 +1,65 @@
-const {bcrypt,jwt,transporter} = require('./modules.js');
+const { bcrypt, jwt, transporter } = require('./modules.js');
 
-// Replacing save() with mock implementation
-// so that calling `save()` doesn’t hit the real database.
+// Mock save() on any Mongoose model instance
 const mockSave = (model, id = '123') => {
-  jest.spyOn(model.prototype, 'save').mockImplementation(function () {
-    this._id = id;
-    return Promise.resolve(this);
-  });
+  if (model.prototype.save) {
+    jest.spyOn(model.prototype, 'save').mockImplementation(function () {
+      this._id = id;
+      return Promise.resolve(this);
+    });
+  } else {
+    model.prototype.save = jest.fn(function () {
+      this._id = id;
+      return Promise.resolve(this);
+    });
+  }
 };
 
-// Mock the `findOne()` method of a Mongoose model
+// Mock findOne() safely
 const mockFindOne = (model, returnValue = null) => {
-  jest.spyOn(model, 'findOne').mockResolvedValue(returnValue);
+  if (model.findOne) {
+    jest.spyOn(model, 'findOne').mockResolvedValue(returnValue);
+  } else {
+    model.findOne = jest.fn().mockResolvedValue(returnValue);
+  }
 };
 
-// Mock the `find()` method of a Mongoose model
+// Mock find() safely
 const mockFind = (model, returnValue = []) => {
-  jest.spyOn(model, 'find').mockResolvedValue(returnValue);
+  if (model.find) {
+    jest.spyOn(model, 'find').mockResolvedValue(returnValue);
+  } else {
+    model.find = jest.fn().mockResolvedValue(returnValue);
+  }
 };
 
-// Mock bcrypt.hash() to always return a fixed hash string
+// Mock bcrypt.hash()
 const mockBcryptHash = (hash = 'hashedPass123') => {
-  jest.spyOn(bcrypt, 'hash').mockResolvedValue(hash);
+  if (bcrypt.hash) {
+    jest.spyOn(bcrypt, 'hash').mockResolvedValue(hash);
+  } else {
+    bcrypt.hash = jest.fn().mockResolvedValue(hash);
+  }
 };
 
-// Mock jwt.sign() to always return a fake token
+// Mock jwt.sign()
 const mockJWT = (token = 'fakeToken123') => {
-  jest.spyOn(jwt, 'sign').mockReturnValue(token);
+  if (jwt.sign) {
+    jest.spyOn(jwt, 'sign').mockReturnValue(token);
+  } else {
+    jwt.sign = jest.fn().mockReturnValue(token);
+  }
 };
 
-// Mock sending email via transporter.sendMail()
+// Mock sending email safely
 const mockSendMail = (resolveValue = true) => {
-  jest.spyOn(transporter, 'sendMail').mockResolvedValue(resolveValue);
+  if (transporter.sendMail) {
+    jest.spyOn(transporter, 'sendMail').mockResolvedValue(resolveValue);
+  } else {
+    transporter.sendMail = jest.fn().mockResolvedValue(resolveValue);
+  }
 };
 
-// Export all mocks so they can be reused in multiple test files
 module.exports = {
   mockSave,
   mockFindOne,
